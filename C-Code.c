98a91503937b71ecc6a93ca87ae8e05a00325b62
HaryanviTau <56214736+HaryanviTau@users.noreply.github.com>
2021-10-01 20:52:33 +0530

    Create C-Code.c

---
+++ C-Code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+   int i,j,spc,rows,k,t=0;
+   printf("Input number of rows : ");
+   scanf("%d",&rows);
+   spc=rows+4-1;
+   for(i=1;i<=rows;i++)
+   {
+         for(k=spc;k>=1;k--)
+            {
+              printf("   ");
+            }
+	   for(j=1;j<=i;j++)
+	   printf("%d\t",t=t+2);
+	printf("\n");
+    spc--;
+   }
+}
