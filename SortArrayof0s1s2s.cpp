eaeec7ad405e38c9921b0da2c11cb5cf7b23e753
Keerthana H K <54075393+KeerthanaHK@users.noreply.github.com>
2021-10-01 21:40:20 +0530

    Create SortArrayof0s1s2s.cpp

---
+++ SortArrayof0s1s2s.cpp
@@ -0,0 +1,68 @@
+//Program to sort an array of elements containing only 0s,1s, and 2s.
+
+#include <stdio.h>
+
+void sort012(int arr[], int n)
+{
+    int i, cnt0 = 0, cnt1 = 0, cnt2 = 0;
+ 
+    // Count the number of 0s, 1s and 2s in the array
+    for (i = 0; i < n; i++) {
+        switch (arr[i]) {
+        case 0:
+            cnt0++;
+            break;
+        case 1:
+            cnt1++;
+            break;
+        case 2:
+            cnt2++;
+            break;
+        }
+    }
+ 
+    // Update the array
+    i = 0;
+ 
+    // Store all the 0s in the beginning
+    while (cnt0 > 0) {
+        arr[i++] = 0;
+        cnt0--;
+    }
+ 
+    // Then all the 1s
+    while (cnt1 > 0) {
+        arr[i++] = 1;
+        cnt1--;
+    }
+ 
+    // Finally all the 2s
+    while (cnt2 > 0) {
+        arr[i++] = 2;
+        cnt2--;
+    }
+ 
+    
+}
+
+int main() {
+
+    int t;
+    scanf("%d", &t);
+
+    while(t--){
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for(int i=0;i<n;i++){
+            scanf("%d", &arr[i]);
+        }
+
+        sort012(arr, n);
+
+        for (int i = 0; i < n; i++)
+            printf("%d ", arr[i]);
+        printf("\n");
+    }
+    return 0;
+}
