3aa54228dd4573439b276388125c72874beff8f1
Fatima Reefat <fatimareefat3@gmail.com>
2021-10-03 12:28:39 +0530

    Added inverse triangle number pattern

--- DFS.cpp
+++ DFS.cpp
@@ -1,50 +1,33 @@
-/* By Aditya Raj */
-// ------------- //
-
-// Program to run DFS (Depth First Search) on a tree with n nodes and (n - 1) edges
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-// adj -> is a Vector of Vector to store connected
-// nodes adjacency list form
-
-vector<vector<int>>adj;
-
-// DFS function
-
-void dfs(int v, int pv)
+vector<int> visited;
+vector<vector<int>> adj;
+void dfs(int n)
 {
-    cout<<v<<" ";       // Printing the visited nodes
-    for(auto i:adj[v])
-    {
-        if(i != pv)
-        {
-            dfs(i, v);
-        }
-    }
+	visited[n] = 1;
+	cout<<n+1<<" ";
+	for(auto i:adj[n])
+	{
+		if(!visited[i])
+		{
+			dfs(i);
+		}
+	}
 }
-
-// Main function
-
 int main()
 {
-    int n;
-    cin>>n;             // Taking input the number of nodes
-    adj.resize(n + 1);
-    for(int i = 0;i < n - 1;i++)
-    {
-        int x, y;
-        cin>>x>>y;
-
-        // Storing nodes in adjacency list form
-
-        adj[x].push_back(y);
-        adj[y].push_back(x);
-    }
-
-    // Calling DFS function considering the root at 1
-
-    dfs(1, -1);
-    return 0;
+	int v,e,x,y;
+	cin>>v>>e;
+	visited.resize(v,0);
+	adj.resize(v);
+	for(int i=0;i<e;i++)
+	{
+		cin>>x>>y;
+		x--;
+		y--;
+		adj[x].push_back(y);
+		adj[y].push_back(x);
+	}
+	dfs(0);
+	//If there are more than 1 connected components run a for loop with number of vertices and dfs(i) in that.
 }
